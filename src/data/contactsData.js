99ebc094a2838665b2c69349b1311707f6f35fb6
kamakshishah <68422664+kamakshishah@users.noreply.github.com>
2023-10-04 19:22:36 -0500

    Update contactsData.js

--- src/data/contactsData.js
+++ src/data/contactsData.js
@@ -1,6 +1,6 @@
 export const contactsData = {
-    email: 'smody@stevens.edu',
-    phone: '+1 (551) 234-7138',
-    address: 'Jersey City, New Jersey, United States - 07307',
+    email: 'shah1018@pnw.edu',
+    phone: '+1 (270) 572-2397',
+    address: '2203 Tanglewood DR, Apt 1B, Hammond, Indiana 46323',
     sheetAPI: 'https://sheetdb.io/api/v1/92mhfs6pgyr34',
 }
