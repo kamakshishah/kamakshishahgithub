2adce191335c88b7a930a24daf29e74e00b50d91
kamakshishah <68422664+kamakshishah@users.noreply.github.com>
2023-10-04 19:28:41 -0500

    Update experienceData.js

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -1,27 +1,26 @@
 export const experienceData = [
     {
         id: 1,
-        company: 'VteamLabs',
-        jobtitle: 'Software Developer Intern',
-        startYear: 'Sep. 2022',
-        endYear: 'Dec. 2022',
+        company: 'Accenture',
+        jobtitle: 'Software Developer',
+        startYear: 'June. 2021',
+        endYear: 'June. 2023',
         duties: <ul>
-                    <li>Implemented 2 features for the OpenEdX course with over 500 students enrolled, using Python, Django, and JavaScript incrementing 35% online learning engagement.</li>
-                    <li>Optimized the existing codebase by refactoring components to uplift performance, applying Agile methodologies and Git which boosted 20% website performance.</li>
-                    <li>Assisted in remodeling a web-based student assessment system which automated the grading process and reduced manual effort by 40%. </li>
-                    <li> Collaborated with business analysts to understand customer needs and develop innovative solutions to enhance customer engagement and satisfaction.</li>
+                    <li>Proficient problem solver with a keen ability to devise innovative solutions and optimize data utilization.</li>
+                    <li>Adroitly executed policy adjustments tailored to client specifications using SQL, joins, key operations ensuring precision and client satisfaction.</li>
+                    <li>Diligently performed bug fixes, conducted code reviews, contributed to major enhancements, enhancing software quality, expertise in task and bug prioritization, fostering efficient project management.</li>
+                    <li>Adept at comprehensive documentation utilizing JIRA, maintaining clear and organized project records.</li>
                 </ul>
     },
     {
         id: 2,
         company: 'Trivia Softwares',
         jobtitle: 'Software Developer Intern',
-        startYear: 'Dec. 2019',
-        endYear: 'Feb 2020',
+        startYear: 'May. 2020',
+        endYear: 'October 2020',
         duties: <ul>
-                    <li>Led a team of 3 and designed an engaging Student Management System applying Tkinter, and worked on Oracle Database to administer database operations on student details.</li>
-                    <li>Scrapped data from various websites adopting APIs and Python libraries such as socket, requests, and beautifulsoup4.</li>
-                    <li>Improved system performance by 8% by working with the testing team to improve the debugging process.</li>
+                    <li>Conceptualized & Created a GUI based student record system.</li>
+                    <li>Technologies used are Swings for GUI, Hibernate Framework for Oracle Database ORM, JFREE Charts for charts</li>
                     {/* <li>Awarded Certificate of Excellence to appreciate my strong leadership, sound character, and passion for innovation.</li> */}
             </ul>
     },
