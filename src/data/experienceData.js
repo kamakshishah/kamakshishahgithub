e4ec4c3a65387be614512454c13ac6d0b271cf09
kamakshishah <68422664+kamakshishah@users.noreply.github.com>
2023-10-04 19:05:24 -0500

    Add files via upload

---
+++ src/data/experienceData.js
@@ -0,0 +1,29 @@
+export const experienceData = [
+    {
+        id: 1,
+        company: 'VteamLabs',
+        jobtitle: 'Software Developer Intern',
+        startYear: 'Sep. 2022',
+        endYear: 'Dec. 2022',
+        duties: <ul>
+                    <li>Implemented 2 features for the OpenEdX course with over 500 students enrolled, using Python, Django, and JavaScript incrementing 35% online learning engagement.</li>
+                    <li>Optimized the existing codebase by refactoring components to uplift performance, applying Agile methodologies and Git which boosted 20% website performance.</li>
+                    <li>Assisted in remodeling a web-based student assessment system which automated the grading process and reduced manual effort by 40%. </li>
+                    <li> Collaborated with business analysts to understand customer needs and develop innovative solutions to enhance customer engagement and satisfaction.</li>
+                </ul>
+    },
+    {
+        id: 2,
+        company: 'Trivia Softwares',
+        jobtitle: 'Software Developer Intern',
+        startYear: 'Dec. 2019',
+        endYear: 'Feb 2020',
+        duties: <ul>
+                    <li>Led a team of 3 and designed an engaging Student Management System applying Tkinter, and worked on Oracle Database to administer database operations on student details.</li>
+                    <li>Scrapped data from various websites adopting APIs and Python libraries such as socket, requests, and beautifulsoup4.</li>
+                    <li>Improved system performance by 8% by working with the testing team to improve the debugging process.</li>
+                    {/* <li>Awarded Certificate of Excellence to appreciate my strong leadership, sound character, and passion for innovation.</li> */}
+            </ul>
+    },
+
+]
