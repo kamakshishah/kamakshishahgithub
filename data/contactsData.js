c8d4867705cc4bbe90d8813e4f54dc2343864706
kamakshishah <68422664+kamakshishah@users.noreply.github.com>
2023-10-04 19:06:11 -0500

    Delete data directory

--- data/contactsData.js
+++
@@ -1,6 +0,0 @@
-export const contactsData = {
-    email: 'smody@stevens.edu',
-    phone: '+1 (551) 234-7138',
-    address: 'Jersey City, New Jersey, United States - 07307',
-    sheetAPI: 'https://sheetdb.io/api/v1/92mhfs6pgyr34',
-}
